f5b6d7b51b4ee5ef5f1e528efd10a093cd00def2
Youcef Madadi <42269089+shirogin@users.noreply.github.com>
2023-10-06 19:27:07 +0100

    update navbar

--- front-end/src/Components/ClientsRow.tsx
+++ front-end/src/Components/ClientsRow.tsx
@@ -1,11 +1,17 @@
-export function ClientsRow({ Clients }: { Clients: { name: string }[] }) {
+export function ClientsRow({ Clients, add = true }: { Clients: { name: string }[]; add?: boolean }) {
 	return (
 		<div className="flex overflow-x-auto no-scrollbar w-full px-6">
+			{add && (
+				<a
+					href="#"
+					className={`flex flex-col text-xs scroll-mx-6 flex-shrink-0 w-16 text-center py-2 rounded-full transition-all `}
+				></a>
+			)}
 			{Clients.map((account, i) => (
 				<a
 					key={i}
 					href="#"
-					className={`flex flex-col text-xs scroll-mx-6 flex-shrink-0 px-2 w-16 text-center py-2  rounded-full transition-all `}
+					className={`flex flex-col text-xs scroll-mx-6 flex-shrink-0 w-16 text-center py-2 rounded-full transition-all `}
 				>
 					<div className="avatar">
 						<div className="w-12 rounded-full">
