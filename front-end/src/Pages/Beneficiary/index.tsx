a7feeb87fb6f55d7f3bb2a379990dedc77cdb225
fatimaBenazzou <b.fati2102@gmail.com>
2023-10-06 20:04:39 +0100

    idk :3

--- front-end/src/Pages/Beneficiary/index.tsx
+++ front-end/src/Pages/Beneficiary/index.tsx
@@ -1,41 +1,42 @@
+// import DZD from "@/utils/Currency";
 import { useState } from "react";
 
-const accountsTypes = [
-	"Investment",
-	<div className="flex gap-2">
-		<img src="/dollar-coin-hd.png" className="w-6 h-6" />
-		Current
-	</div>,
-	"Savings",
-	"Term deposit",
+const tabs = [
+	"Other",
+	"Own Account",
 ];
 
+
 const Beneficiary = () => {
-	const [currentAccount, setCurrentAccount] = useState(1);
+	const [currentTab, setCurrentTab] = useState(0);
 
   return (
     <>
-    <div className="flex  overflow-x-auto no-scrollbar w-full px-6">
-      {accountsTypes.map((account, i) => (
-        <a
-          key={i}
-          href="#"
-          onClick={(e) => {
-            setCurrentAccount(i);
-            // scroll to the current element show fully
-            e.currentTarget.scrollIntoView({ behavior: "smooth", block: "center" });
-          }}
-          className={` scroll-mx-6 flex-shrink-0 px-6 py-2  rounded-full transition-all  ${
-            currentAccount === i ? "text-black bg-white font-bold" : "text-white"
-          }`}
-        >
-          {account}
-        </a>
-      ))}
-    </div>
-    <div className="container"></div>
-  </>
-  )
-}
+			<div className="w-f flex flex-col justify-center items-center pt-8 ">
+				<div className="flex w-fit px-6 gap-4">
+					{tabs.map((tab, i) => (
+						<a
+							key={i}
+							href="#"
+							onClick={() => {
+								setCurrentTab(i);
+							}}
+							className={` scroll-mx-6 flex-shrink-0 px-6 py-2  rounded-full transition-all  ${
+								currentTab === i ? "text-black bg-white font-bold" : "text-white"
+							}`}
+						>
+							{tab}
+						</a>
+					))}
+				</div>
+				<div className="flex flex-col w-full px-6 mt-12">
+					<h2 className="text-2xl font-semibold text-center">Balance</h2>
+					{/* <p className="text-4xl font-semibold text-center text-white mt-2">{DZD.EN.format(tabs[currentTab])}</p> */}
+				</div>
+			</div>
+		</>
+  );
+};
+
 
 export default Beneficiary
