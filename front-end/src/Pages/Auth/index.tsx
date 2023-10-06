3dbee1f4c9230f1fc56851f1c8801ce71ec047d5
fatimaBenazzou <b.fati2102@gmail.com>
2023-10-06 14:49:00 +0100

    facee

--- front-end/src/Pages/Auth/index.tsx
+++ front-end/src/Pages/Auth/index.tsx
@@ -1,12 +1,21 @@
 import Page from "@/Components/Page";
 import PinCode from "@/Components/PinCode";
-import { useEffectOnce } from "@/hooks";
+import { useEffectOnce, useModal } from "@/hooks";
+import { MODAL_BODY_TYPES } from "@/utils/globalConstantUtil";
 
 const Auth = () => {
-
+	const { openModal } = useModal();
+/* 
     useEffectOnce(() => {
-        
-    })
+        console.log("kech hadja")
+        openModal({
+            bodyType: MODAL_BODY_TYPES.FACE_RECOGNITION,
+            title: "FACE RECOGNITION",
+        })
+        // .then(() => {
+            // Refetch();
+        // });
+    }) */
     return (
         <Page
             title="Set up PIN Code"
