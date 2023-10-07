bb101bbac0e07298d9f9d75b73ffc58c9b2113d6
Kessir wassim <53373646+Muk4g3n@users.noreply.github.com>
2023-10-07 10:51:19 +0100

    add support route

--- front-end/src/Routes/index.tsx
+++ front-end/src/Routes/index.tsx
@@ -12,6 +12,7 @@ import Chat from "@/Pages/Chat";
 const Beneficiary = lazy(() => import("@/Pages/Beneficiary"));
 const Error404 = lazy(() => import("@/Pages/Errors/Error404"));
 const AuthVerification = lazy(() => import("@/Pages/AuthVerification"));
+const Support = lazy(() => import("@/Pages/Support"));
 
 const Layout = lazy(() => import("@/Layout"));
 const Logout = lazy(() => import("@/Pages/Logout"));
@@ -20,56 +21,60 @@ const LogIn = lazy(() => import("@/Pages/LogIn"));
 const Dashboard = lazy(() => import("@/Pages/protected/Dashboard"));
 
 const Router = () => {
-	const { user } = useUser();
-	return useRoutes([
-		{ index: true, element: <Navigate to={user ? "/app/home" : "/onboarding"} /> },
-		{
-			path: "app",
-			element: user ? <AppLayout /> : <Navigate to="/login" />,
-			children: [
-				{ path: "", element: <Navigate to={"/app/home"} /> },
-				{ path: "home", element: <Home /> },
-				{
-					path: "dashboard", // the url
-					element: <Dashboard />,
-				},
-				{ path: "beneficiary", element: <Beneficiary /> },
-				{ path: "confirm", element: <Confirm /> },
-				{ path: "chat", element: <Chat /> },
-				{
-					path: "auth",
-					element: <Auth />,
-				},
-				{
-					path: "auth-verification",
-					element: <AuthVerification />,
-				},
-				{ path: "spending", element: <Spending /> },
-				{ path: "*", element: <Error404 /> },
-			],
-		},
-		{
-			path: "/*",
-			element: <Layout />,
-			children: [
-				{
-					path: "onboarding",
-					element: <OnBoarding />,
-				},
+  const { user } = useUser();
+  return useRoutes([
+    {
+      index: true,
+      element: <Navigate to={user ? "/app/home" : "/onboarding"} />,
+    },
+    {
+      path: "app",
+      element: user ? <AppLayout /> : <Navigate to="/login" />,
+      children: [
+        { path: "", element: <Navigate to={"/app/home"} /> },
+        { path: "home", element: <Home /> },
+        {
+          path: "dashboard", // the url
+          element: <Dashboard />,
+        },
+        { path: "beneficiary", element: <Beneficiary /> },
+        { path: "confirm", element: <Confirm /> },
+        { path: "chat", element: <Chat /> },
+        {
+          path: "auth",
+          element: <Auth />,
+        },
+        {
+          path: "auth-verification",
+          element: <AuthVerification />,
+        },
+        { path: "spending", element: <Spending /> },
+        { path: "support", element: <Support /> },
+        { path: "*", element: <Error404 /> },
+      ],
+    },
+    {
+      path: "/*",
+      element: <Layout />,
+      children: [
+        {
+          path: "onboarding",
+          element: <OnBoarding />,
+        },
 
-				{
-					path: "login",
-					element: user ? <Navigate to="/app/auth" /> : <LogIn />,
-				},
+        {
+          path: "login",
+          element: user ? <Navigate to="/app/auth" /> : <LogIn />,
+        },
 
-				{
-					path: "logout",
-					element: user ? <Logout /> : <Navigate to="/login" />,
-				},
+        {
+          path: "logout",
+          element: user ? <Logout /> : <Navigate to="/login" />,
+        },
 
-				{ path: "*", element: <Error404 /> },
-			],
-		},
-	]);
+        { path: "*", element: <Error404 /> },
+      ],
+    },
+  ]);
 };
 export default Router;
