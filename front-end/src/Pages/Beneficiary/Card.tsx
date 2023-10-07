0a378a01f6c0ffe14f2310690d4f1585b3cfb912
fatimaBenazzou <b.fati2102@gmail.com>
2023-10-07 01:11:24 +0100

    beneficiary

--- front-end/src/Pages/Beneficiary/Card.tsx
+++ front-end/src/Pages/Beneficiary/Card.tsx
@@ -1,8 +1,44 @@
 
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
 
-export default Card
+// type AccountType = {
+//   icon: string;
+//   color: string;
+//   title: string;
+//   sub: string;
+// };
+
+type CardProps = {
+  title: string;
+  sub: string;
+  icon: string;
+  color: string;
+  selected: boolean;
+  onSelect: () => void;
+};
+
+const Card: React.FC<CardProps> = ({ title, sub, icon, color, selected, onSelect }) => {
+  return (
+      <div className={`card shadow-xl`} onClick={onSelect}>
+          <div className=" card-body flex flex-row gap-4 items-center p-4">
+              <div className="avatar">
+                  <div className={`w-16 h-16 p-4 rounded-full bg-${color}`}>
+                      <img src={icon} alt={title} />
+                  </div>
+              </div>
+              <div className="flex flex-col">
+                  <h1 className="card-title text-lg">{title}</h1>
+                  <p className="text-sm">{sub}</p>
+              </div>
+              <input
+                  type="radio"
+                  name="accountSelection"
+                  className="radio checked:bg-primary"
+                  checked={selected}
+                  
+              />
+          </div>
+      </div>
+  );
+};
+
+export default Card;
--- front-end/src/Pages/Beneficiary/index.tsx
+++ front-end/src/Pages/Beneficiary/index.tsx
@@ -1,5 +1,8 @@
 // import DZD from "@/utils/Currency";
+import useNavbar from "@/hooks/useNavbar";
 import { useState } from "react";
+import Other from "./Other";
+import OwnAccount from "./OwnAccount";
 
 const tabs = [
 	"Other",
@@ -9,10 +12,27 @@ const tabs = [
 
 const Beneficiary = () => {
 	const [currentTab, setCurrentTab] = useState(0);
+  useNavbar(false)
+
+  const renderTabContent = () => {
+    switch (currentTab) {
+      case 0:
+        return (
+          <Other />
+        );
+      case 1:
+        return (
+          <OwnAccount />
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
     <>
-			<div className="w-f flex flex-col justify-center items-center pt-8 ">
+			<div className="w-full h-full flex flex-col justify-center items-center pt-8 gap-12">
+        <h1 className="font-bold text-2xl">Beneficiary</h1>
 				<div className="flex w-fit px-6 gap-4">
 					{tabs.map((tab, i) => (
 						<a
@@ -29,10 +49,9 @@ const Beneficiary = () => {
 						</a>
 					))}
 				</div>
-				<div className="flex flex-col w-full px-6 mt-12">
-					<h2 className="text-2xl font-semibold text-center">Balance</h2>
-					{/* <p className="text-4xl font-semibold text-center text-white mt-2">{DZD.EN.format(tabs[currentTab])}</p> */}
-				</div>
+				<div className="flex flex-col w-full h-full">
+          {renderTabContent()}
+        </div>
 			</div>
 		</>
   );
